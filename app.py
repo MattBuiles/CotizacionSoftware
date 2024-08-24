13114cf7dd8da000fd11e2d3077460d909bee9c3
Mateo Builes Duque <141962207+MattBuiles@users.noreply.github.com>
2024-08-24 13:05:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 # Ruta Absoluta para evitar problemas de acceso
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
